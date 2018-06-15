20459999c6100e2cc81780d2970dea8e27a9633c
DESKTOP-LJ4L6HP\jimev <jimevegar@gmail.com>
2018-06-15 17:25:03 -0600

    Cuarto commit, abarca el punto 5 del examen, en el cual se crea una clase Jugador con dos atributos: nombre y mano.

---
+++ src/cr/ac/cenfotec/examen1/logica/Jugador.java
@@ -0,0 +1,41 @@
+package cr.ac.cenfotec.examen1.logica;
+
+import java.util.ArrayList;
+
+public class Jugador {
+
+	private String nombre;
+	private ArrayList<Carta> mano = new ArrayList<Carta>();
+	
+	public Jugador() {
+		
+	}
+	
+	public Jugador(String nombre, ArrayList<Carta> mano) {
+		super();
+		this.nombre = nombre;
+		this.mano = mano;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public ArrayList<Carta> getMano() {
+		return mano;
+	}
+
+	public void setMano(ArrayList<Carta> mano) {
+		this.mano = mano;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", mano=" + mano + "]";
+	}
+	
+}
