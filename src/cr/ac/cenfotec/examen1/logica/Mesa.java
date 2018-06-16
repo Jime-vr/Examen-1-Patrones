0a9bd7409cd44f57de97d743c8b6a00a82741e18
DESKTOP-LJ4L6HP\jimev <jimevegar@gmail.com>
2018-06-15 18:10:21 -0600

    Quinto commit, abarca el punto 6.1 en el cual se crea una clase Mesa con los atributos: repartidorMesa y jugadores.

---
+++ src/cr/ac/cenfotec/examen1/logica/Mesa.java
@@ -0,0 +1,11 @@
+package cr.ac.cenfotec.examen1.logica;
+
+import java.util.ArrayList;
+
+public class Mesa {
+
+	Repartidor repartidorMesa = new Repartidor();
+	ArrayList <Jugador> jugadores = new ArrayList<Jugador>();
+	
+	
+}
